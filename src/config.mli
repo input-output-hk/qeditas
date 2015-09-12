e127f77601e871c05f58dbcd3d638e5bd7e0077a
Bill White <billwhite@bitmessage.ch>
2015-09-12 17:25:50 -0400

    added config.mli to hold location of the default data directory

---
+++ src/config.mli
@@ -0,0 +1,5 @@
+(* Copyright (c) 2015 The Qeditas developers *)
+(* Distributed under the MIT software license, see the accompanying
+   file COPYING or http://www.opensource.org/licenses/mit-license.php. *)
+
+val datadir : string ref
