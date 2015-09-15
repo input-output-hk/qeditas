1b8c36218ccdcf0d65f09a72093c8a22a34da0e2
Bill White <billwhite@bitmessage.ch>
2015-09-15 14:54:21 -0400

    added placeholder files for net, qeditascli, qeditasd

---
+++ src/net.ml
@@ -0,0 +1,8 @@
+(* Copyright (c) 2015 The Qeditas developers *)
+(* Distributed under the MIT software license, see the accompanying
+   file COPYING or http://www.opensource.org/licenses/mit-license.php. *)
+
+open Assets
+open Tx
+open Ctre
+open Block
