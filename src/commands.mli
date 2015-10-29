83fbefd2668ab3898ed163fe614659a1a408b523
Bill White <billwhite@bitmessage.ch>
2015-10-29 14:06:12 -0600

    changing interface for commands to include pubkey in wallet endorsements and to use txpool instead of txqueue

--- src/commands.mli
+++ src/commands.mli
@@ -14,18 +14,18 @@ val addnode : string -> int -> bool
 ***)
 
 val walletkeys : (big_int * bool * (big_int * big_int) * string * hashval * string) list ref
-val walletendorsements : (payaddr * payaddr * int * bool * signat) list ref
+val walletendorsements : (payaddr * payaddr * (big_int * big_int) * int * bool * signat) list ref
 val walletwatchaddrs : addr list ref
 val stakingassets : (p2pkhaddr * hashval * int64 * obligation * int64) list ref
 val storagetrmassets : (hashval option * tm * tp * hashval * hashval) list ref
 val storagedocassets : (pubaddr * hashval * hashval option * pdoc * hashval * hashval) list ref
 val recenttxs : (hashval,Tx.stx) Hashtbl.t
-val txqueue : (hashval,Tx.stx) Hashtbl.t
+val txpool : (hashval,Tx.stx) Hashtbl.t
 
 val load_currentframe : unit -> frame
 val save_currentframe : frame -> unit
 val load_recenttxs : unit -> unit
-val load_txqueue : unit -> unit
+val load_txpool : unit -> unit
 val load_wallet : unit -> unit
 val save_wallet : unit -> unit
 
