21ec7affc38e7e574c896bf4dd155844acc221bd
Bill White <billwhite@bitmessage.ch>
2015-11-16 17:22:34 -0700

    removed printmd from interface

--- src/ripemd160.mli
+++ src/ripemd160.mli
@@ -5,7 +5,6 @@
 open Sha256
 
 type md = int32 * int32 * int32 * int32 * int32
-val printmd : md -> unit
 val ripemd160_md256 : md256 -> md
 val hexstring_md : string -> md
 val md_hexstring : md -> string
