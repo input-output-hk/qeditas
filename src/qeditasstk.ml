318f95fa4847d59ac930c1d9e609fe5410fa7f48
Trent Russell <russell@willmathforcrypto.com>
2016-01-15 18:56:11 +0000

    staker was not reading command line arguments or config file

--- src/qeditasstk.ml
+++ src/qeditasstk.ml
@@ -2,9 +2,6 @@
 (* Distributed under the MIT software license, see the accompanying
    file COPYING or http://www.opensource.org/licenses/mit-license.php. *)
 
-(* Code for the staking process, intended to be started and controlled by qeditasd *)
-let stakelog = open_out_gen [Open_append;Open_creat] 0o600 (Filename.concat (Ctre.datadir()) "qstakelog");;
-
 open Big_int;;
 open Ser;;
 open Hash;;
@@ -13,6 +10,14 @@ open Assets;;
 open Ctre;;
 open Block;;
 open Net;;
+open Setconfig;;
+
+(* Code for the staking process, intended to be started and controlled by qeditasd *)
+let stakelog = open_out_gen [Open_append;Open_creat] 0o600 (Filename.concat (Ctre.datadir()) "qstakelog");;
+
+datadir_from_command_line(); (*** if -datadir=... is on the command line, then set Config.datadir so we can find the config file ***)
+process_config_file();
+process_config_args(); (*** settings on the command line shadow those in the config file ***)
 
 set_binary_mode_in stdin true;;
 set_binary_mode_out stdout true;;
